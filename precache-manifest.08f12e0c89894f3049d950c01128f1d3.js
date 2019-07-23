5348a3631122cd15c4610921004abbbe71dd4e8a
rohanajith <rohanajith5090@gmail.com>
2019-07-23 22:59:14 +0530

    Updates

---
+++ precache-manifest.08f12e0c89894f3049d950c01128f1d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fb1c83574abcae7286de9f1fbb3fa97",
+    "url": "/Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "4cfe53ec20fd35ecc77b",
+    "url": "/Tic-Tac-Toe/static/css/main.003f5f65.chunk.css"
+  },
+  {
+    "revision": "cee52af2c70d1e58d144",
+    "url": "/Tic-Tac-Toe/static/js/2.12d49320.chunk.js"
+  },
+  {
+    "revision": "4cfe53ec20fd35ecc77b",
+    "url": "/Tic-Tac-Toe/static/js/main.5530f507.chunk.js"
+  },
+  {
+    "revision": "10779627d94960e7b271",
+    "url": "/Tic-Tac-Toe/static/js/runtime~main.adeb2bb3.js"
+  }
+]);
